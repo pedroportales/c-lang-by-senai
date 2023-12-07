82ae6ab44a825df344f73931d9e9df31370e2133
Pedro Portales <pedroportales@riseup.net>
2023-12-07 15:44:16 -0300

    pilha start

---
+++ avulsos/pilha.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdio.h>
+
+typedef struct pilha
+{
+    int dado;
+    struct pilha *prox
+} celula;
+
+void imprimir();
+void push(int);
+void pop();
+
+celula *top = NULL;
+
+int main(int argc, char const *argv[])
+{
+    int n, opcao;
+    return 0;
+}
+
